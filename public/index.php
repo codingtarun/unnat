81c95c4d439b1d3bc0cca00757c410a5101dea85
coding.tarun <coding.tarun@gmail.com>
2024-06-19 18:47:05 +0530

    Basic files & folder setup

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Index
+|--------------------------------------------------------------------------
+|
+| Entry point fo the appliction
+|
+*/
+
+/**
+ * Class Example
+ *
+ * This is an example class to show the comment snippits
+ *
+ * @package Example
+ * @author TC
+ */
+
+echo "Entry point of the frameowrk";
