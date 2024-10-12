15c14b8395edec93ca601a2e795022c313171d4e
coding.tarun <coding.tarun@gmail.com>
2024-10-12 21:04:05 +0530

    Update App.php

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -12,6 +12,20 @@
 
 declare(strict_types=1);
 
+/*
+|--------------------------------------------------------------------------
+| Namespace
+|--------------------------------------------------------------------------
+|
+| A way of grouping related classes, interfaces, functions togeather.
+| Namespace can be considered as a 'Virtual' folder that keeps related 
+| files togeather.
+| If two classes have the same name but are used in different contexts,
+| namespace prevents conflicts.
+|
+|
+*/
+
 namespace Framework;
 
 class App
